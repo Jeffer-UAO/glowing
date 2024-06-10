ce052a0dd5f304c164b7e915432ed9e9679d28f0
JefferDevop <somadesarrollo@gmail.com>
2024-06-10 06:39:50 -0500

    Change presentation

--- src/components/ListProducts/Available/Available.js
+++ src/components/ListProducts/Available/Available.js
@@ -76,10 +76,10 @@ export function Available(props) {
       <div className={styles.product}>
         <div className={styles.price}>
           {product.productData.price2 > 0 && (
-            <label>Con descuento $ {format(product.productData.price2)}</label>
+            <label>Por Mayor $ {format(product.productData.price2)}</label>
           )}
           {product.productData.price1 > 0 && (
-            <label>Sin descuento $ {format(product.productData.price1)}</label>
+            <label>Por Menor $ {format(product.productData.price1)}</label>
           )}
         </div>
       </div>
